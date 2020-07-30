127f5dbbb716ba12c1b6b10d693615f0049d9dfb
dkos <kosdejan@yahoo.com>
2020-07-30 19:11:18 +0200

    working get

--- src/cache.rs
+++ src/cache.rs
@@ -1,13 +1,13 @@
 use std::collections::HashMap;
-use std::time::{SystemTime, UNIX_EPOCH};
+
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use actix_web::http::{HeaderMap, HeaderName, HeaderValue};
 use actix_web::HttpRequest;
-use serde::{Deserialize, Serialize};
 use lru_time_cache::LruCache;
-use std::sync::Mutex;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct CachedRequest {
     http_version: String,
     method: String,
@@ -19,21 +19,46 @@ pub struct CachedRequest {
     ip: String,
 }
 
-pub struct Cache {
-    pub cache: Mutex<LruCache<String, Vec<CachedRequest>>>
+pub struct CacheManager {
+    cache: LruCache<String, Vec<CachedRequest>>,
 }
 
+impl CacheManager {
+    pub(crate) fn new() -> Self {
+        CacheManager {
+            cache: LruCache::<String, Vec<CachedRequest>>::with_expiry_duration(
+                Duration::from_secs(15 * 60),
+            ),
+        }
+    }
 
+    pub(crate) fn store(&mut self, req: HttpRequest) -> CachedRequest {
+        let cached_req = to_cached_request(req);
+        if let Some(r) = self.cache.get_mut(&cached_req.path) {
+            r.push(cached_req.clone())
+        } else {
+            self.cache
+                .insert(cached_req.path.clone(), vec![cached_req.clone()]);
+        }
 
-pub fn to_cached_request(req: HttpRequest) -> CachedRequest {
+        cached_req
+    }
+
+    pub(crate) fn retrieve(&mut self, path: &String) -> Option<Vec<CachedRequest>> {
+        let key = &remove_base_path(path.as_str());
+        self.cache.remove(key)
+    }
+}
+
+fn to_cached_request(req: HttpRequest) -> CachedRequest {
     let time = current_time_ms();
-    let headers = to_hash_map(req.headers());
+    let headers = headers_to_map(req.headers());
     let http_version = format!("{:?}", req.version());
     let method = format!("{:?}", req.method());
     let query_string = String::from(req.query_string());
     let conn_info = req.connection_info();
     let ip = String::from(conn_info.remote().unwrap_or("localhost"));
-    let path = String::from(req.path());
+    let path = remove_base_path(req.path());
     let body = String::from("");
 
     CachedRequest {
@@ -48,6 +73,10 @@ pub fn to_cached_request(req: HttpRequest) -> CachedRequest {
     }
 }
 
+fn remove_base_path(path: &str) -> String {
+    path[5..path.len()].to_string()
+}
+
 fn current_time_ms() -> u128 {
     if let Ok(dur) = SystemTime::now().duration_since(UNIX_EPOCH) {
         dur.as_millis()
@@ -56,25 +85,19 @@ fn current_time_ms() -> u128 {
     }
 }
 
-fn to_hash_map(headers: &HeaderMap) -> HashMap<String, String> {
+fn headers_to_map(headers: &HeaderMap) -> HashMap<String, String> {
     headers
         .iter()
-        .map(|(name, value)| header_as_str_tuple(name, value))
+        .filter_map(|(name, value)| header_as_str_tuple(name, value))
         .collect()
 }
 
-fn header_as_str_tuple(
-    name: &HeaderName,
-    value: &HeaderValue,
-) -> (String, String) {
-    (name.to_string(),
-     match value.to_str() {
-         Ok(s) => s.to_string(),
-         Err(e) => {
-             error!("Error converting header = {} to string, e = {} ", name, e);
-             format!("{}", e)
-         }
-     }
-    )
+fn header_as_str_tuple(name: &HeaderName, value: &HeaderValue) -> Option<(String, String)> {
+    match value.to_str() {
+        Ok(s) => Some((name.to_string(), s.to_string())),
+        Err(e) => {
+            error!("Error converting header = {} to string, e = {} ", name, e);
+            None
+        }
+    }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,58 @@
 #[macro_use]
 extern crate log;
 
-use std::collections::HashMap;
+use std::borrow::BorrowMut;
+
 use std::sync::Mutex;
-use std::time::{SystemTime, UNIX_EPOCH};
 
-use actix_web::{App, HttpMessage, HttpRequest, HttpResponse, HttpServer, web};
-use actix_web::get;
-use actix_web::http::{HeaderMap, HeaderName, HeaderValue};
-use actix_web::http::header::ToStrError;
+use actix_web::{get, HttpMessage};
+
 use actix_web::middleware::Logger;
+use actix_web::post;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use log::LevelFilter;
-use simplelog::{Config, TerminalMode, TermLogger};
+use simplelog::{Config, TermLogger, TerminalMode};
 
-use crate::cache::{CachedRequest, to_cached_request, Cache};
+use crate::cache::{CacheManager, CachedRequest};
 
 mod cache;
 
-struct AppStateWithCounter {
-    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
-}
-
 #[get("/push/*")]
-async fn push_get(data: web::Data<Cache>, req: HttpRequest) -> HttpResponse {
-    //store_data(data, req);
-
-    HttpResponse::Ok().json(to_cached_request(req))
+async fn push_get(manager: web::Data<Mutex<CacheManager>>, req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().json(store_data(manager, req))
 }
 
-fn store_data(data: web::Data<Cache>, _req: HttpRequest) {
-    let c = data.cache.lock().unwrap();
-
-    // let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    // *counter += 1; // <- access counter inside MutexGuard
+#[post("/push/*")]
+async fn push_post(manager: web::Data<Mutex<CacheManager>>, req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().json(store_data(manager, req))
 }
 
-// time = 1596040068821
-// header map = HeaderMap { inner: {"accept": One("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"), "sec-fetch-user": One("?1"), "accept-language": One("en-GB,en;q=0.9,hr-HR;q=0.8,hr;q=0.7,en-US;q=0.6,bs;q=0.5"), "sec-fetch-mode": One("navigate"), "sec-fetch-site": One("none"), "accept-encoding": One("gzip, deflate, br"), "cookie": One("_ga=GA1.1.833966493.1591460296"), "host": One("localhost:8088"), "user-agent": One("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"), "connection": One("keep-alive"), "upgrade-insecure-requests": One("1"), "sec-fetch-dest": One("document")} }
-// version = HTTP/1.1
-// method = GET
-// query_string = a=x&b=3
-// ip = 127.0.0.1:52908
-// scheme = http
-// path = /push/1/1
+#[get("/poll/*")]
+async fn poll_get(mut manager: web::Data<Mutex<CacheManager>>, req: HttpRequest) -> HttpResponse {
+    let mut man = manager.borrow_mut().lock().unwrap();
+    HttpResponse::Ok().json(man.retrieve(&req.path().to_string()))
+}
 
+fn store_data(mut manager: web::Data<Mutex<CacheManager>>, req: HttpRequest) -> CachedRequest {
+    let mut man = manager.borrow_mut().lock().unwrap();
+    man.store(req)
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     TermLogger::init(LevelFilter::Info, Config::default(), TerminalMode::Mixed).unwrap();
 
-    HttpServer::new(|| {
+    let global_state = web::Data::new(Mutex::new(CacheManager::new()));
+    HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .app_data(web::Data::new(AppStateWithCounter {
-                counter: Mutex::new(0),
-            }))
+            .app_data(global_state.clone())
             .service(push_get)
+            .service(push_post)
+            .service(poll_get)
     })
-        .bind("127.0.0.1:8088")?
-        .run()
-        .await
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
